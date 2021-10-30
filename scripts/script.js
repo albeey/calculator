b2ea04a7dfa13bbb5018ab6f00d16a3059733bfd
Alberto <malberto24@icloud.com>
2021-10-29 21:21:37 -0500

    Finish calculator layout

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// Math functions
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+// Operate function
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
+}

08fb654375f67882ed8c3b801ab7b71b7ee8aed0
Alberto <malberto24@icloud.com>
2021-10-30 22:56:34 -0500

    Add digit, decimal and delete functionality

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,42 @@ function operate(operator, num1, num2) {
       return divide(num1, num2);
   }
 }
+
+const screen = document.querySelector(".display");
+const digits = document.querySelectorAll(".digit");
+let displayValue = "0";
+
+// Event listener for the digit buttons
+digits.forEach(digit => {
+  digit.addEventListener("click", updateDisplay);
+})
+
+function updateDisplay() {
+  const digit = this.textContent;
+  if (screen.textContent === "0") screen.textContent = "";
+  displayValue = screen.textContent += digit;
+}
+
+// Decimal separator
+const decimalBttn = document.querySelector(".decimal");
+decimalBttn.addEventListener("click", addDecimalSeparator)
+
+function addDecimalSeparator() {
+  let decimal = this.textContent;
+  if (displayValue.includes(".")) decimal = "";
+  displayValue = screen.textContent += decimal;
+}
+
+// Backspace
+const backspaceBttn = document.querySelector(".backspace");
+backspaceBttn.addEventListener("click", deleteLastDigit);
+
+function deleteLastDigit() {
+  const currentVal = screen.textContent;
+  let newVal = currentVal.slice(0, -1);
+
+  if (newVal === "") newVal = "0";
+
+  screen.textContent = newVal;
+  displayValue = newVal;
+}
